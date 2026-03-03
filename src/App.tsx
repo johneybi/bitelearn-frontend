e78bf73c964086f3fc826df33982ee0cbd7e6ff9
aroin <bboroin917@gmail.com>
2026-03-03 15:53:44 +0900

    refactor: 라우팅 로직 Router.tsx로 분리 #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from '@/pages/Home';
-import Login from '@/pages/Login';
-import Signup from '@/pages/Signup';
-import AuthCallback from '@/pages/AuthCallback';
-import NotFound from '@/pages/NotFound';
+import Router from '@/router/Router';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-
-        <Route path="/oauth/callback/:provider" element={<AuthCallback />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
