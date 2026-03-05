54cc1d83b1b7b5092cc34941e90c181304dc0273
aroin <bboroin917@gmail.com>
2026-03-05 11:18:23 +0900

    refactor: 레이아웃 기반 라우팅 구조로 재구성 #6

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import RootLayout from '@/layouts/RootLayout';
+import AppLayout from '@/layouts/AppLayout';
+import AuthLayout from '@/layouts/AuthLayout';
+
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import Signup from '@/pages/Signup';
@@ -10,14 +14,21 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-
-        <Route path="/oauth/callback/:provider" element={<AuthCallback />} />
-
-        <Route path="*" element={<NotFound />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/oauth/callback/:provider"
+              element={<AuthCallback />}
+            />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
