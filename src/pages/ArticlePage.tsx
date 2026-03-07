3580b765a854dfc0585a70eb5e8f6eb04f8d941b
aroin <bboroin917@gmail.com>
2026-03-08 03:29:19 +0900

    remove: 임시 아티클 페이지 삭제 #18

--- src/pages/ArticlePage.tsx
+++
@@ -1,7 +0,0 @@
-export default function ArticlePage() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <p className="text-sm font-medium text-slate-500">아티클 페이지</p>
-    </div>
-  );
-}
