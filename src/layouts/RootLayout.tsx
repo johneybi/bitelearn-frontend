154dc31ae4243c67c7f905d94c5d93ecfc411b58
aroin <bboroin917@gmail.com>
2026-03-08 03:00:20 +0900

    feat: 아티클 디테일 페이지 UI 구현 #18

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export default function RootLayout() {
   return (
-    <div className="min-h-dvh bg-neutral-100">
-      <div className="mx-auto flex min-h-dvh w-full max-w-app flex-col bg-white">
+    <div className="h-dvh bg-neutral-100">
+      <div className="mx-auto flex h-full w-full max-w-app flex-col bg-white">
         <Outlet />
       </div>
     </div>
