bb7e96fb12210ee77984e0471e542fb4ef3d410c
aroin <bboroin917@gmail.com>
2026-03-23 02:16:55 +0900

    feat: 약관 페이지 및 마이페이지 약관 링크 연결 #71

---
+++ src/constants/terms.ts
@@ -0,0 +1,5 @@
+export const SERVICE_TERMS_URL =
+  'https://www.notion.so/goormkdx/Bitelearn-328c0ff4ce318063a705cb0171894b50?source=copy_link';
+
+export const PRIVACY_TERMS_URL =
+  'https://www.notion.so/goormkdx/328c0ff4ce31800ab5b3e96142b8a118?source=copy_link';
